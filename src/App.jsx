9665db465f53c110e2a04cbc8b6d2bb94012b1a6
iknoor12 <imiknoor@gmail.com>
2025-08-13 16:14:06 +0530

    2nd-commit Form Functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,33 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+
+    localStorage.setItem("userName", name);
+    localStorage.setItem("userEmail", email);
+
+    alert("User data saved in localStorage!");
+
+    setName("");
+    setEmail("");
+  };
 
   return (
     <>
       <h1>User Registration Form</h1>
 
-      <form>
-        <label for='name'>Name: <input type="text" id='nameInput' placeholder='Enter your name..' name='Username' /></label>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='name'>Name: <input type="text" id='nameInput' placeholder='Enter your name..' name='Username' value={name}
+          onChange={e => setName(e.target.value)} /></label>
 
-        <label for='email'>E-mail: <input type="email" id='emailInput' placeholder='Write email..' name='Email' /></label>
+        <label htmlFor='email'>E-mail: <input type="email" id='emailInput' placeholder='Write email..' name='Email' value={email} 
+          onChange={e => setEmail(e.target.value)}/></label>
 
-        <div className='submitDiv'><button>Submit</button></div>
+        <div className='submitDiv'> <button type='submit'>Submit</button> </div>
       </form>
       
     </>
